c256a51cfd9600beb28ca523bcfe09c551d79fca
Ivo Lopes <ilopes@openwt.com>
2019-08-08 09:08:40 +0200

    first commit

---
+++ src/main/java/com/boot/bike/AppBikeApplication.java
@@ -0,0 +1,13 @@
+package com.boot.bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBikeApplication.class, args);
+	}
+
+}
